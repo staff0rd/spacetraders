8160411ef974be8d7686c226226a7e9d092924ff
Stafford Williams <stafford.williams@gmail.com>
2021-03-18 23:18:00 +1100

    fix playerMachine

--- src/machines/playerMachine.ts
+++ src/machines/playerMachine.ts
@@ -1,101 +1,16 @@
 import { assign, createMachine } from "xstate";
 import { User } from "../api/User";
 import { apiMachine, ApiResult } from "./apiMachine";
-import { GetUserResponse, GetTokenResponse } from "../api";
+import { GetUserResponse, getUser, getToken } from "../api";
+import { newPlayerName } from "../newPlayerName";
 
 type PlayerContext = {
   token?: string;
   user?: User;
 };
 
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-// export const playerMachine = createMachine(
-//   {
-//     id: "player",
-//     initial: "initial",
-//     context: { token: "initial" },
-//     states: {
-//       initial: {
-//         entry: () => console.log("initial"),
-//         always: {
-//           actions: "log",
-//           target: "idle",
-//         },
-//       },
-//       idle: {
-//         entry: () => console.log("idle"),
-//         always: [
-//           {
-//             target: "getToken",
-//             cond: "isInitial",
-//           },
-//           { target: "loaded", cond: "isNotInitial" },
-//         ],
-//       },
-//       getToken: {
-//         entry: () => console.log("getToken"),
-//         invoke: {
-//           src: "getToken",
-//           onDone: {
-//             target: "idle",
-//             actions: "assignToken",
-//           },
-//         },
-//       },
-//       loaded: {
-//         type: "final",
-//         entry: () => console.log("loaded"),
-//       },
-//     },
-//   },
-//   {
-//     actions: {
-//       // assignToken: () => {
-//       //   console.log("assigning");
-//       //   assign(() => ({ token: "a thing" }));
-//       // },
-//       assignToken: assign(() => ({ token: "a thing" })),
-//     },
-//     services: {
-//       getUser: () =>
-//         apiMachine(() => Promise.resolve({ user: { username: "ay" } })),
-//       getToken: () => {
-//         console.log("getToken promise");
-//         return Promise.resolve({ token: "from-service" });
-//       },
-//     },
-//     guards: {
-//       isInitial: (context: { token: string }) => context.token === "initial",
-//       isNotInitial: (context) => context.token !== "initial",
-//     },
-//   }
-// );
-
-/*
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 const getCachedPlayer = (): PlayerContext => {
   const player = localStorage.getItem("player");
-  console.log("player", player);
   if (player) return JSON.parse(player);
   else return {};
 };
@@ -103,15 +18,15 @@ const getCachedPlayer = (): PlayerContext => {
 export const playerMachine = createMachine(
   {
     id: "player",
-    initial: "idle",
+    initial: "checkStorage",
     context: {} as PlayerContext,
     states: {
       checkStorage: {
-        entry: ["assignCachedPlayer", () => console.warn("checkStorage")],
+        entry: ["assignCachedPlayer", (c) => console.warn("checkStorage", c)],
         always: "idle",
       },
       idle: {
-        entry: ["assignCachedPlayer", () => console.warn("idle")],
+        entry: [() => console.warn("idle")],
         always: [
           { target: "getToken", cond: (context) => !context.token },
           { target: "getUser", cond: (context) => !!context.token },
@@ -127,7 +42,7 @@ export const playerMachine = createMachine(
           API_RESULT: {
             target: "cachePlayer",
             actions: [
-              assign({ something: (c, e: any) => e.result }),
+              assign({ apiResult: (c, e: any) => e.result }),
               (c: any) => console.log("after cache", c),
             ],
           } as any,
@@ -143,15 +58,18 @@ export const playerMachine = createMachine(
         invoke: {
           src: "getUser",
           onError: "idle",
-        },
-        on: {
-          API_RESULT: {
+          onDone: {
             target: "loaded",
-            actions: "assignPlayer",
+            actions: assign<PlayerContext, any>({
+              user: (c, e) => e.data.user,
+            }),
           },
         },
       },
-      loaded: {},
+      loaded: {
+        type: "final",
+        entry: (c) => console.warn("loaded", c),
+      },
     },
   },
   {
@@ -162,21 +80,12 @@ export const playerMachine = createMachine(
       }) as any,
     },
     services: {
-      getUser: () =>
-        apiMachine(() => {
-          console.warn("called get user");
-          return Promise.resolve({ user: { username: "ay" } });
-        }),
-      getToken: () =>
-        apiMachine(() => {
-          console.warn("called getToken");
-          return Promise.resolve({ token: "124", username: "hi" });
-        }),
-      cachePlayer: (async (c: PlayerContext) => {
+      getUser: (c: PlayerContext) => getUser(c.token!, c.user!.username),
+      getToken: () => apiMachine(() => getToken(newPlayerName())),
+      cachePlayer: async (c: PlayerContext) => {
         console.log("cached player", c);
-        localStorage.setItem("player", JSON.stringify((c as any).something));
-        await sleep(2000);
-      }) as any,
+        localStorage.setItem("player", JSON.stringify((c as any).apiResult));
+      },
     },
   }
 );
