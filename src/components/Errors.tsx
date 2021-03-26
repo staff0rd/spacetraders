90a4eea223a9688a4b5bcb4244164e17491f0e06
Stafford Williams <stafford.williams@gmail.com>
2021-03-26 22:18:12 +1100

    errors

--- src/components/Errors.tsx
+++ src/components/Errors.tsx
@@ -1,3 +1,68 @@
+import { useLiveQuery } from "dexie-react-hooks";
+import db from "../data";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import Paper from "@material-ui/core/Paper";
+import { DateTime } from "luxon";
+import { makeStyles } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const useStyles = makeStyles((theme) => ({
+  data: {
+    margin: 0,
+  },
+  when: {
+    width: 100,
+  },
+}));
+
 export const Errors = () => {
-  return <div>Errors</div>;
+  const classes = useStyles();
+  const errors = useLiveQuery(() => db.apiErrors.reverse().limit(50).toArray());
+
+  if (!errors) return <CircularProgress color="primary" size={24} />;
+
+  return (
+    <TableContainer component={Paper}>
+      <Table size="small" aria-label="Trades">
+        <TableHead>
+          <TableRow>
+            <TableCell>Code</TableCell>
+            <TableCell>Message / Path</TableCell>
+            <TableCell>Data</TableCell>
+            <TableCell className={classes.when}>When</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {errors.map((error) => (
+            <TableRow key={error.id}>
+              <TableCell component="th" scope="row">
+                {error.code}
+              </TableCell>
+              <TableCell>
+                {error.message}
+                <br />
+                {error.path}
+              </TableCell>
+              <TableCell>
+                {error.data && (
+                  <pre className={classes.data}>
+                    {JSON.stringify(error.data, null, 2)}
+                  </pre>
+                )}
+              </TableCell>
+
+              <TableCell className={classes.when} align="right">
+                {DateTime.fromISO(error.created).toRelative()}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
--- src/components/Trades.tsx
+++ src/components/Trades.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLiveQuery } from "dexie-react-hooks";
 import db from "../data";
 import TableContainer from "@material-ui/core/TableContainer";
@@ -8,6 +9,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
 import Tooltip from "@material-ui/core/Tooltip";
 import Paper from "@material-ui/core/Paper";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { DateTime } from "luxon";
 import { TradeType } from "../data/ITrade";
 import NumberFormat from "react-number-format";
@@ -15,6 +17,10 @@ import green from "@material-ui/core/colors/green";
 import blue from "@material-ui/core/colors/blue";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles((theme) => ({
   buy: {
@@ -34,72 +40,95 @@ export const Trades = () => {
       .limit(20)
       .toArray()
   );
+  const [good, setGood] = useState("");
 
-  if (!trades) return null; // Still loading.
+  const goods = useLiveQuery(() => db.trades.orderBy("good").uniqueKeys());
+
+  if (!trades) return <CircularProgress color="primary" size={24} />;
 
   return (
-    <TableContainer component={Paper}>
-      <Table size="small" aria-label="Trades">
-        <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-            <TableCell align="right">Location</TableCell>
-            <TableCell align="right">Qty</TableCell>
-            <TableCell align="right">Good</TableCell>
-            <TableCell align="right">Cost</TableCell>
-            <TableCell align="right">Profit</TableCell>
-            <TableCell align="right">When</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {trades.map((trade) => (
-            <TableRow
-              className={clsx({
-                [classes.sell]: trade.type === TradeType.Sell,
-                [classes.buy]: trade.type === TradeType.Buy,
-              })}
-              key={trade.id}
-            >
-              <TableCell component="th" scope="row">
-                {trade.type === TradeType.Buy ? "BUY" : "SELL"}
-              </TableCell>
-              <TableCell align="right">{trade.location}</TableCell>
-              <TableCell align="right">
-                <NumberFormat
-                  value={trade.quantity}
-                  thousandSeparator=","
-                  displayType="text"
-                />
-              </TableCell>
-              <TableCell align="right">{trade.good}</TableCell>
-              <TableCell align="right">
-                <NumberFormat
-                  value={trade.cost}
-                  thousandSeparator=","
-                  displayType="text"
-                  prefix="$"
-                />
-              </TableCell>
-              <TableCell align="right">
-                <NumberFormat
-                  value={trade.profit}
-                  thousandSeparator=","
-                  displayType="text"
-                  prefix="$"
-                />
-                {trade.type === TradeType.Buy && (
-                  <Tooltip title="Estimated">
-                    <span>*</span>
-                  </Tooltip>
-                )}
-              </TableCell>
-              <TableCell align="right">
-                {DateTime.fromISO(trade.timestamp).toRelative()}
-              </TableCell>
+    <>
+      {goods && (
+        <FormControl>
+          <InputLabel id="filter-by-good-label">Good</InputLabel>
+          <Select
+            labelId="filter-by-good-label"
+            id="filter-by-good"
+            displayEmpty
+            value={good}
+            variant="filled"
+            onChange={(event) => setGood(event.target.value as string)}
+          >
+            <MenuItem value={""}>All</MenuItem>
+            {goods!.map((good: any) => (
+              <MenuItem value={good}>{good}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      )}
+      <TableContainer component={Paper}>
+        <Table size="small" aria-label="Trades">
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell align="right">Location</TableCell>
+              <TableCell align="right">Qty</TableCell>
+              <TableCell align="right">Good</TableCell>
+              <TableCell align="right">Cost</TableCell>
+              <TableCell align="right">Profit</TableCell>
+              <TableCell align="right">When</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {trades.map((trade) => (
+              <TableRow
+                className={clsx({
+                  [classes.sell]: trade.type === TradeType.Sell,
+                  [classes.buy]: trade.type === TradeType.Buy,
+                })}
+                key={trade.id}
+              >
+                <TableCell component="th" scope="row">
+                  {trade.type === TradeType.Buy ? "BUY" : "SELL"}
+                </TableCell>
+                <TableCell align="right">{trade.location}</TableCell>
+                <TableCell align="right">
+                  <NumberFormat
+                    value={trade.quantity}
+                    thousandSeparator=","
+                    displayType="text"
+                  />
+                </TableCell>
+                <TableCell align="right">{trade.good}</TableCell>
+                <TableCell align="right">
+                  <NumberFormat
+                    value={trade.cost}
+                    thousandSeparator=","
+                    displayType="text"
+                    prefix="$"
+                  />
+                </TableCell>
+                <TableCell align="right">
+                  <NumberFormat
+                    value={trade.profit}
+                    thousandSeparator=","
+                    displayType="text"
+                    prefix="$"
+                  />
+                  {trade.type === TradeType.Buy && (
+                    <Tooltip title="Estimated">
+                      <span>*</span>
+                    </Tooltip>
+                  )}
+                </TableCell>
+                <TableCell align="right">
+                  {DateTime.fromISO(trade.timestamp).toRelative()}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
