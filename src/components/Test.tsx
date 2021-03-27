d2e517ec4d2cda6bedf63b519d94c9c64512dc9b
Stafford Williams <stafford.williams@gmail.com>
2021-03-28 09:06:08 +1100

    remove test

--- src/components/Test.tsx
+++
@@ -1,7 +0,0 @@
-import { useMachine } from "@xstate/react";
-import { testMachine } from "../machines/testMachine";
-
-export const Test = () => {
-  useMachine(testMachine, { devTools: true });
-  return <></>;
-};
