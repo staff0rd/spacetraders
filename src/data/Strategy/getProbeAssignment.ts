19f8bf2277dd065cc564e14aa425f8ca77f0bcbb
Stafford Williams <stafford.williams@gmail.com>
2021-04-05 17:22:33 +1000

    probe by shortest distance

--- src/data/Strategy/getProbeAssignment.ts
+++ src/data/Strategy/getProbeAssignment.ts
@@ -1,18 +1,30 @@
 import Bottleneck from "bottleneck";
 import db from "../";
+import { getDistance } from "../../machines/getDistance";
+import { IProbe } from "../IProbe";
 
 const limiter = new Bottleneck({ maxConcurrent: 1 });
 export const getProbeAssignment = async (system: string, shipId: string) => {
   return limiter.schedule(() => getAssignment(system, shipId));
 };
 const getAssignment = async (system: string, shipId: string) => {
-  const assignments = await db.probes.toArray();
-  const assignment = assignments.find((p) => p.shipId === shipId);
+  const probes = await db.probes.toArray();
+  const assignment = probes.find((p) => p.shipId === shipId);
   if (assignment) return assignment;
 
-  const newAssignment = assignments.find(
-    (p) => p.location.startsWith(system) && p.shipId === undefined
-  );
+  const ship = await db.ships.get(shipId);
+  if (!ship?.location) {
+    const message = "Unexpected: No location on ship";
+    console.warn(message);
+    throw new Error(message);
+  }
+  const from: IProbe = probes.find((p) => p.location === ship?.location)!;
+  const unassigned = probes
+    .filter((p) => p.location.startsWith(system) && p.shipId === undefined)
+    .map((p) => ({ ...p, distance: getDistance(from.x, from.y, p.x, p.y) }))
+    .sort((a, b) => a.distance - b.distance);
+
+  const newAssignment = unassigned[0];
 
   if (newAssignment) {
     db.probes.put({
