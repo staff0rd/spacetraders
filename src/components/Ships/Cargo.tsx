fc40c63f37ba6e55452001f461916b1bbb9dc5b0
Stafford Williams <stafford.williams@gmail.com>
2021-04-14 01:19:22 +1000

    icons on shipview

--- src/components/Ships/Cargo.tsx
+++ src/components/Ships/Cargo.tsx
@@ -1,6 +1,7 @@
 import { Ship } from "../../api/Ship";
 import { CircularProgress } from "@material-ui/core";
 import { DataTable } from "../DataTable";
+import { GoodIcon } from "components/Trades/GoodIcon";
 
 type Props = {
   ship?: Ship;
@@ -9,7 +10,11 @@ type Props = {
 const CargoComponent = ({ ship }: Props) => {
   if (!ship) return <CircularProgress />;
   const columns = ["Cargo", "Qty", "㎥"];
-  const rows = ship.cargo.map((c) => [c.good, c.quantity, c.totalVolume]);
+  const rows = ship.cargo.map((c) => [
+    <GoodIcon good={c.good} />,
+    c.quantity,
+    c.totalVolume,
+  ]);
   return <DataTable title="Cargo" rows={rows} columns={columns} />;
 };
 export default CargoComponent;
